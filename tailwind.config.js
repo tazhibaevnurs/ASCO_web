10b265ccb22aa0f1591bdb7267afa0a48391894b
neo_nurs <neocoindevelopers@gmail.com>
2026-04-13 10:36:53 +0600

    optimization adopted

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.html', './static/**/*.js'],
+  prefix: 'tw-',
+  corePlugins: { preflight: false },
+  theme: { extend: {} },
+  plugins: [],
+};
